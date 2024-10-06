9c1d5a91d271fa1b70484c4292608816e1950051
tyler-torr <tylertorrella@gmail.com>
2024-10-06 14:01:52 -0700

    Added Pity inputs
    You can now put in the pity you start out with

--- pull_simulator.gd
+++ pull_simulator.gd
@@ -5,21 +5,23 @@ const HSR_BANNER = {
 	"base_pull_rate": 0.006,
 	"soft_pity_start": 74,
 	"hard_pity": 90,
-	"premium_chance": 0.564, # According to 棵平衡樹, HSR's character banner actually has 56.4% odds of winning a 5*
+	"premium_chance": 0.564, # According to 棵平衡樹, HSR's character banner has 56.4% odds to win
 	"wep_base_pull_rate": 0.008,
-	"wep_soft_pity_start": 64, # Temporary value
+	"wep_soft_pity_start": 63, # Temporary value
 	"wep_hard_pity": 80,
 	"wep_premium_chance": 0.75
 }
 
 var banner = HSR_BANNER
 
-@onready var desired_five_stars_input = $FiveStarCharacterInput
-@onready var desired_five_star_weapons_input = $FiveStarWeaponInput
 @onready var available_pulls_input = $AvailablePullsInput
 @onready var available_currency_input = $AvailableCurrencyInput
 @onready var simulation_runs_input = $SimulationRunsInput
+@onready var desired_five_stars_input = $FiveStarCharacterInput
+@onready var character_pity_input = $CharacterPityInput
 @onready var guarantee_input = $GuaranteeInput
+@onready var desired_five_star_weapons_input = $FiveStarWeaponInput
+@onready var weapon_pity_input = $WeaponPityInput
 @onready var weapon_guarantee_input = $WeaponGuaranteeInput
 
 
@@ -35,7 +37,6 @@ func pull(pity: int) -> bool:
 	elif pity >= banner["soft_pity_start"]:
 		current_pull_rate += 0.06 * (pity - banner["soft_pity_start"] + 1)
 	
-	current_pull_rate = clamp(current_pull_rate, banner["base_pull_rate"], 1.0)
 	return current_pull_rate >= randf()
 
 
@@ -45,9 +46,8 @@ func wep_pull(pity: int) -> bool:
 	if pity == banner["wep_hard_pity"]:
 		current_pull_rate = 1.0
 	elif pity >= banner["wep_soft_pity_start"]:
-		current_pull_rate += 0.06 * (pity - banner["wep_soft_pity_start"] + 1)
+		current_pull_rate += 0.08 * (pity - banner["wep_soft_pity_start"] + 1)
 	
-	current_pull_rate = clamp(current_pull_rate, banner["wep_base_pull_rate"], 1.0)
 	return current_pull_rate >= randf()
 
 
@@ -59,15 +59,16 @@ func wep_fifty_fifty(guarantee: bool) -> bool:
 	return guarantee or randf() <= banner["wep_premium_chance"]
 
 
-func simulate_pulls(desired_five_stars: int, desired_five_star_weapons: int, available_pulls: int, simulation_runs: int, guarantee: bool, weapon_guarantee: bool) -> float:
+func simulate_pulls(available_pulls: int, simulation_runs: int, desired_five_stars: int, character_pity: int,
+		guarantee: bool, desired_five_star_weapons: int, weapon_pity: int, weapon_guarantee: bool) -> float:
 	var total_success = 0
 	
 	for i in range(simulation_runs):
 		var five_stars_pulled = 0
 		var weapons_pulled = 0
 		var remaining_pulls = available_pulls
-		var pity = 0
-		var wep_pity = 0
+		var pity = character_pity
+		var wep_pity = weapon_pity
 		var current_guarantee = guarantee
 		var wep_guarantee = weapon_guarantee
 		
@@ -86,6 +87,7 @@ func simulate_pulls(desired_five_stars: int, desired_five_star_weapons: int, ava
 				else:
 					pity += 1
 			
+			# Weapon banners
 			elif weapons_pulled < desired_five_star_weapons:
 				if wep_pull(wep_pity):
 					wep_pity = 0
@@ -104,16 +106,19 @@ func simulate_pulls(desired_five_stars: int, desired_five_star_weapons: int, ava
 
 
 func _on_run_button_pressed() -> void:
-	var desired_five_stars = int(desired_five_stars_input.value)
-	var desired_five_star_weapons = int(desired_five_star_weapons_input.value)
 	var available_pulls = int(available_pulls_input.value)
 	var available_currency = int(available_currency_input.value)
 	var simulation_runs = int(simulation_runs_input.value)
+	var desired_five_stars = int(desired_five_stars_input.value)
+	var character_pity = int(character_pity_input.value)
 	var guarantee = guarantee_input.is_pressed()
+	var desired_five_star_weapons = int(desired_five_star_weapons_input.value)
+	var weapon_pity = int(weapon_pity_input.value)
 	var weapon_guarantee = weapon_guarantee_input.is_pressed()
 	
 	available_pulls += (available_currency / 160)
 	
-	var average_success = simulate_pulls(desired_five_stars, desired_five_star_weapons, available_pulls, simulation_runs, guarantee, weapon_guarantee)
+	var average_success = simulate_pulls(available_pulls, simulation_runs, desired_five_stars, character_pity,
+			guarantee, desired_five_star_weapons, weapon_pity, weapon_guarantee)
 	
 	$ResultLabel.text = "Success Rate: " + str(average_success * 100) + "%"
