a1a09a18f6c6c5973ecc8c4a35f4dd2e5abe7f78
tyler-torr <tylertorrella@gmail.com>
2024-10-05 20:29:02 -0700

    Working on adding multiple banner types
    Currently pretty messy implementation, not very happy with it right now.

--- pull_simulator.gd
+++ pull_simulator.gd
@@ -1,72 +1,88 @@
 extends Control
 
 
-const HSR_CHARACTER_BANNER = {
-	"base_pull_rate": 0.006,
-	"soft_pity_start": 74,
-	"hard_pity": 90,
-	"premium_chance": 0.564 # According to 棵平衡樹, HSR's character banner actually has 56.4% odds of winning a 5*
+const HSR_BANNER = {
+	"CHARACTER": {
+		"FIVE_STAR_RATE": 0.006,
+		"SOFT_PITY": 74,
+		"HARD_PITY": 90,
+		"FIFTY_FIFTY": 0.564 # According to 棵平衡樹, HSR's character banner actually has 56.4% odds of winning a 5*
+	},
+	"WEAPON": {
+		"FIVE_STAR_RATE": 0.008,
+		"SOFT_PITY": 64, # Temporary
+		"HARD_PITY": 80,
+		"FIFTY_FIFTY": 0.75
+	}
 }
 
-const HSR_WEAPON_BANNER = {
-	"base_pull_rate": 0.008,
-	"soft_pity_start": 64, # Temporary value
-	"hard_pity": 80,
-	"premium_chance": 0.75
-}
-
-var banner = HSR_CHARACTER_BANNER
-var weapon_banner = HSR_WEAPON_BANNER
+var banner = HSR_BANNER
 
 @onready var desired_five_stars_input = $FiveStarCharacterInput
 @onready var desired_five_star_weapons_input = $FiveStarWeaponInput
 @onready var available_pulls_input = $AvailablePullsInput
-@onready var available_currency_input = $AvailableCurrencyInput
+@onready var available_gems_input = $AvailableGemsInput
 @onready var simulation_runs_input = $SimulationRunsInput
-@onready var guarantee_input = $GuaranteeInput
+@onready var character_guarantee_input = $CharacterGuaranteeInput
+@onready var weapon_guarantee_input = $WeaponGuaranteeInput
 
 
 func ready() -> void:
 	pass
 
 
-func pull(pity: int) -> bool:
-	var current_pull_rate = banner["base_pull_rate"]
+func pull_character(banner: Dictionary, pity: int) -> bool:
+	var current_pull_rate = banner["CHARACTER"]["FIVE_STAR_RATE"]
 	
-	if pity == banner["hard_pity"]:
+	if pity >= banner["CHARACTER"]["HARD_PITY"]:
 		current_pull_rate = 1.0
-	elif pity >= banner["soft_pity_start"]:
-		current_pull_rate += 0.06 * (pity - banner["soft_pity_start"] + 1)
+	elif pity >= banner["CHARACTER"]["SOFT_PITY"]:
+		current_pull_rate += 0.06 * (pity - banner["SOFT_PITY"] + 1)
 	
-	current_pull_rate = clamp(current_pull_rate, banner["base_pull_rate"], 1.0)
+	current_pull_rate = clamp(current_pull_rate, banner["CHARACTER"]["FIVE_STAR_RATE"], 1.0) # Necessary?
 	return current_pull_rate >= randf()
 
 
-func fifty_fifty(guarantee: bool) -> bool:
-	return guarantee or randf() <= banner["premium_chance"]
+func pull_weapon(banner: Dictionary, pity: int) -> bool:
+	var current_pull_rate = banner["WEAPON"]["FIVE_STAR_RATE"]
+	
+	if pity >= banner["WEAPON"]["HARD_PITY"]:
+		current_pull_rate = 1.0
+	elif pity >= banner["WEAPON"]["SOFT_PITY"]:
+		current_pull_rate += 0.06 * (pity - banner["SOFT_PITY"] + 1)
+	
+	current_pull_rate = clamp(current_pull_rate, banner["WEAPON"]["FIVE_STAR_RATE"], 1.0) # Necessary?
+	return current_pull_rate >= randf()
 
 
-func simulate_pulls(desired_five_stars: int, available_pulls: int, simulation_runs: int, guarantee: bool) -> float:
+# Check whether the 50/50 is won or not
+func fifty_fifty(banner: Dictionary, guarantee: bool) -> bool:
+	return guarantee or randf() <= banner["FIFTY_FIFTY"]
+
+
+func simulate_pulls(banner: Dictionary, desired_five_stars: int, available_pulls: int, simulation_runs: int,
+		character_guarantee: bool, weapon_guarantee: bool) -> float:
 	var total_success = 0
 	
 	for i in range(simulation_runs):
-		var five_stars_pulled = 0
+		var characters_pulled = 0
 		var remaining_pulls = available_pulls
 		var pity = 0
-		var current_guarantee = guarantee
+		var current_character_guarantee = character_guarantee
 		
 		while remaining_pulls != 0:
 			remaining_pulls -= 1
-			if pull(pity):
+			
+			if pull_character(banner, pity):
 				pity = 0
-				if fifty_fifty(current_guarantee):
-					current_guarantee = false
-					five_stars_pulled += 1
-					if five_stars_pulled >= desired_five_stars:
+				if fifty_fifty(banner, current_character_guarantee):
+					current_character_guarantee = false
+					characters_pulled += 1
+					if characters_pulled >= desired_five_stars:
 						total_success += 1
 						break
 				else:
-					current_guarantee = true
+					current_character_guarantee = true
 			else:
 				pity += 1
 	
@@ -76,13 +92,14 @@ func simulate_pulls(desired_five_stars: int, available_pulls: int, simulation_ru
 func _on_run_button_pressed() -> void:
 	var desired_five_stars = int(desired_five_stars_input.value)
 	var available_pulls = int(available_pulls_input.value)
-	var available_currency = int(available_currency_input.value)
+	var available_gems = int(available_gems_input.value)
 	var simulation_runs = int(simulation_runs_input.value)
-	var guarantee = guarantee_input.is_pressed()
+	var character_guarantee = character_guarantee_input.is_pressed()
+	var weapon_guarantee = weapon_guarantee_input.is_pressed()
 	
-	available_pulls += (available_currency / 160)
-	# test test test
+	available_pulls += (available_gems / 160)
 	
-	var average_success = simulate_pulls(desired_five_stars, available_pulls, simulation_runs, guarantee)
+	var average_success = simulate_pulls(desired_five_stars, available_pulls, simulation_runs, character_guarantee,
+			weapon_guarantee)
 	
 	$ResultLabel.text = "Success rate: " + str(average_success * 100) + "%"
