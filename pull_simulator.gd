82357fe4e75fcfc3ad948fc91be62d0708d52c58
tyler-torr <tylertorrella@gmail.com>
2024-10-12 02:45:48 -0700

    More groundwork for 4* pulling
    Currently not working but don't know why it's 3 AM and I'm tired. Simulating 4* and 5* pulling at the same time is becoming increasingly complex. May need to split this across multiple scripts

--- pull_simulator.gd
+++ pull_simulator.gd
@@ -49,6 +49,7 @@ const WUWA_BANNER = {
 }
 
 var banner
+var copies_preference = 0
 
 @onready var available_pulls_input = $AvailablePullsInput
 @onready var available_currency_input = $AvailableCurrencyInput
@@ -66,13 +67,27 @@ func ready() -> void:
 	pass
 
 
-func pull(banner_type: String, pity: int) -> bool:
+func pull(banner_type: String, pity: int, four_star_pity: int) -> String:
 	var current_pull_rate = banner[banner_type]["PULL_RATE"]
-	if pity == banner[banner_type]["HARD_PITY"]:
+	var current_four_star_pull_rate = banner["4-STAR"]["PULL_RATE"]
+	var roll = randf()
+	
+	if pity >= banner[banner_type]["HARD_PITY"]:
 		current_pull_rate = 1.0
 	elif pity >= banner[banner_type]["SOFT_PITY_START"]:
 		current_pull_rate += banner[banner_type]["PITY_RATE"] * (pity - banner[banner_type]["SOFT_PITY_START"] + 1)
-	return current_pull_rate >= randf()
+	
+	if four_star_pity >= banner["4-STAR"]["HARD_PITY"]:
+		current_pull_rate = 1.0
+	elif four_star_pity >= banner["4-STAR"]["SOFT_PITY_START"]:
+		current_four_star_pull_rate += banner["4-STAR"]["PITY_RATE"] * (pity - banner[banner_type]["SOFT_PITY_START"] + 1)
+	
+	if current_pull_rate >= roll:
+		return "5-STAR"
+	elif current_four_star_pull_rate >= roll:
+		return "4-STAR"
+	else:
+		return "3-STAR"
 
 
 func fifty_fifty(banner_type: String, guarantee: bool) -> bool:
@@ -83,10 +98,13 @@ func gem_conversion(conversion_rate: int, remaining_gems: int) -> int:
 	return remaining_gems / conversion_rate
 
 
-func simulate_banner(banner_type: String, five_stars_pulled: int, pity: int, guarantee: bool,
-					remaining_gems: int) -> Array:
-	if pull(banner_type, pity):
+func simulate_banner(banner_type: String, five_stars_pulled: int, pity: int, four_star_pity: int, guarantee: bool,
+					four_star_guarantee: bool, remaining_gems: int) -> Array:
+	var pull = pull(banner_type, pity, four_star_pity)
+	
+	if pull == "5-STAR":
 		pity = 0
+		four_star_pity += 1
 		remaining_gems += banner["OTHER"]["5-STAR_GEM_GAIN"]
 		if fifty_fifty(banner_type, guarantee):
 			guarantee = false
@@ -95,7 +113,19 @@ func simulate_banner(banner_type: String, five_stars_pulled: int, pity: int, gua
 			guarantee = true
 	else:
 		pity += 1
-	return [five_stars_pulled, pity, guarantee, remaining_gems]
+		if pull == "4-STAR":
+			if fifty_fifty("4-STAR", four_star_guarantee):
+				four_star_guarantee = false
+				if copies_preference == 0:
+					remaining_gems += banner["OTHER"]["4-STAR_GEM_GAIN"]
+				elif copies_preference == 1:
+					remaining_gems += banner["OTHER"]["4-STAR_EXCESS_GEM_GAIN"]
+			else:
+				four_star_guarantee = true
+				remaining_gems += banner["OTHER"]["4-STAR_GEM_GAIN"]
+		else:
+			four_star_pity += 1
+	return [five_stars_pulled, pity, four_star_pity, guarantee, four_star_guarantee, remaining_gems]
 
 
 func simulate_pulls(available_pulls: int, available_currency: int, available_gems: int, simulation_runs: int,
@@ -112,10 +142,12 @@ func simulate_pulls(available_pulls: int, available_currency: int, available_gem
 		
 		var pity = character_pity
 		var wep_pity = weapon_pity
-		# var four_star_pity = 0
+		var four_star_pity = 0
+		var wep_four_star_pity = 0
 		
 		var current_guarantee = guarantee
 		var wep_guarantee = weapon_guarantee
+		var four_star_guarantee = false
 		
 		while remaining_pulls > 0 || remaining_gems >= banner["OTHER"]["GEM_CONVERSION_RATE"]:
 			if (remaining_gems >= banner["OTHER"]["GEM_CONVERSION_RATE"]):
@@ -125,18 +157,22 @@ func simulate_pulls(available_pulls: int, available_currency: int, available_gem
 			
 			# Character banners
 			if five_stars_pulled < desired_five_stars:
-				var result: Array = simulate_banner("CHARACTER", five_stars_pulled, pity, current_guarantee, remaining_gems)
+				var result: Array = simulate_banner("CHARACTER", five_stars_pulled, pity, four_star_pity, current_guarantee, four_star_guarantee, remaining_gems)
 				five_stars_pulled = result[0]
 				pity = result[1]
-				current_guarantee = result[2]
-				remaining_gems = result[3]
+				four_star_pity = result[2]
+				current_guarantee = result[3]
+				four_star_guarantee = result[4]
+				remaining_gems = result[5]
 			# Weapon banners
 			if weapons_pulled < desired_five_star_weapons:
-				var result: Array = simulate_banner("WEAPON", weapons_pulled, wep_pity, wep_guarantee, remaining_gems)
+				var result: Array = simulate_banner("WEAPON", weapons_pulled, wep_pity, wep_four_star_pity, wep_guarantee, four_star_guarantee, remaining_gems)
 				weapons_pulled = result[0]
 				wep_pity = result[1]
-				wep_guarantee = result[2]
-				remaining_gems = result[3]
+				wep_four_star_pity = result[2]
+				wep_guarantee = result[3]
+				four_star_guarantee = result[4]
+				remaining_gems = result[5]
 		
 		if (five_stars_pulled >= desired_five_stars) and (weapons_pulled >= desired_five_star_weapons):
 			total_success += 1
@@ -168,3 +204,10 @@ func _on_option_button_item_selected(index: int) -> void:
 		banner = HSR_BANNER
 	elif index == 1:
 		banner = WUWA_BANNER
+
+
+func _on_copies_button_item_selected(index: int) -> void:
+	if index == 0:
+		copies_preference = 0
+	elif index == 1:
+		copies_preference = 1
